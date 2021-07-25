d1739ca7bc34681660192cda64fae7fbd7ef661e
nashmazlan <87932199+nashmazlan@users.noreply.github.com>
2021-07-25 23:54:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("Let's settle this! Refresh Me!);
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "images/dice" + randomNumber1 + ".png";
 var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
