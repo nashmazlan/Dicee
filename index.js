87a326829a26b6dc7360f6b8a251c2b174e75a49
nashmazlan <87932199+nashmazlan@users.noreply.github.com>
2021-07-25 20:27:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomeDice
 
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelectorAll("h1")[0].innerHTML = "Option 1 Wins";
-  document.querySelectorAll("h2")[0].innerHTML = "";
+  document.querySelector("h1").innerHTML = "Option 1 Wins";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelectorAll("h1")[0].innerHTML = "Option 2 Wins";
-  document.querySelectorAll("h2")[0].innerHTML = "";
+  document.querySelector("h1").innerHTML = "Option 2 Wins";
 } else if (randomNumber1 == randomNumber2) {
-  document.querySelectorAll("h1")[0].innerHTML = "Draw! Roll Again~"
-  document.querySelectorAll("h2")[0].innerHTML = "";
+  document.querySelector("h1").innerHTML = "Draw! Roll Again~"
 }
