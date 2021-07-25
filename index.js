129b4011ea6f2cf79052e0db448eada8b184f2bf
nashmazlan <87932199+nashmazlan@users.noreply.github.com>
2021-07-25 18:43:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomeDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomeDiceImage2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelectorAll("h1")[0].innerHTML = "Player 1 Wins";
+  document.querySelectorAll("h2")[0].innerHTML = "";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelectorAll("h1")[0].innerHTML = "Player 2 Wins";
+  document.querySelectorAll("h2")[0].innerHTML = "";
+} else if (randomNumber1 == randomNumber2) {
+  document.querySelectorAll("h1")[0].innerHTML = "Draw! Roll Again~"
+  document.querySelectorAll("h2")[0].innerHTML = "";
+}
